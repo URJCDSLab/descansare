69f3ce27b0e50a277f02f7a988b1b2d63ac8c6a1
Vacek <rodetlas@gmail.com>
2020-06-24 21:08:01 +0200

    Consulta a tabla de perfiles de usuario

--- src/data/db_data.py
+++ src/data/db_data.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 # Connect to the database.
 con = pymysql.connect(host='localhost',
-                      user='victor',
-                      password='machine1$',
-                      db='pharaoh',
+                      user='root',
+                      password='',
+                      db='flex',
                       charset='utf8mb4',
-                      port=6666,
+                      port=3306,
                       cursorclass=pymysql.cursors.DictCursor)
 
 # prepare a cursor object using cursor() method
@@ -19,17 +19,8 @@ cursor.execute("SELECT VERSION()")
 try:
     with con.cursor() as cursor:
         # Read all records
-        sql = """select o.id as id, null as destino1, c.name as canal, 
-        s.name as servicio from opportunities o left join opportunities_destinations 
-        d on o.id = d.opportunity left join channels c on o.channel = c.id 
-        left join services s on o.service=s.id where o.organization = 1 and 
-        (d.isOrigin = 1 and not exists (select * from opportunities_destinations d2 
-        where o.id = d2.opportunity and d2.isOrigin = 0)) union select o.id as id, 
-        d.text as destino1, c.name as canal, s.name as servicio from opportunities o 
-        left join opportunities_destinations d on o.id = d.opportunity 
-        left join channels c on o.channel = c.id left join services s on o.service=s.id 
-        where o.organization = 1 and (d.isOrigin = 0 or not exists 
-        (select null from opportunities_destinations d2 where o.id = d2.opportunity)) ORDER BY `id` ASC"""
+        sql = """select * from perfiles_usuario 
+       """
         cursor.execute(sql)
         result = cursor.fetchall()
 finally:
@@ -37,9 +28,6 @@ finally:
 
 # Parse dict to pandas
 df = pd.DataFrame.from_dict(result)
-# Set correct index
-df.set_index('id', inplace=True)
-# Replace null by 'selecciona'
-df.canal[df.canal.isnull()] = 'selecciona'
-# Save data to predict
-df.to_parquet('data/external/db_data.parquet')
+
+# Save data raw
+df.to_parquet('data/raw/flex_perfiles_usuario.parquet')
