13e48e7097ee3f685ba5cc27950f552a323a111f
Vacek <rodetlas@gmail.com>
2020-06-25 11:25:47 +0200

    código de paquetización

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='descansare',
     packages=find_packages(),
     version='0.1.0',
     description='Proyecto Descansare en colaboración con Pixelabs',
