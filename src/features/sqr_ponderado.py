ee8003b61d95dcccbdaebd83e74543ee5311a673
vacek-ace <rodetlas@gmail.com>
2020-10-15 15:22:36 +0200

    Añado copia de dataframe

--- src/features/sqr_ponderado.py
+++ src/features/sqr_ponderado.py
@@ -3,11 +3,14 @@ import numpy as np
 
 
 def sqr_ponderado(data, drop=True, min=3):
-    data['notaUsuario'] = data['notaUsuario']*10
+    
+    df = data.copy()
+    
+    df['notaUsuario'] = df['notaUsuario']*10
 
-    freq_notas_perfiles = data[data['notaUsuario'].notnull()]['idPerfiles'].value_counts()
+    freq_notas_perfiles = df[df['notaUsuario'].notnull()]['idPerfiles'].value_counts()
 
-    freq_sesiones = data['idPerfiles'].value_counts()
+    freq_sesiones = df['idPerfiles'].value_counts()
 
     freq_sesiones.name = 'freq_sesiones'
 
@@ -21,16 +24,16 @@ def sqr_ponderado(data, drop=True, min=3):
 
     df_notas['freq_rel'] = np.round(df_notas.freq_notas / df_notas.freq_sesiones,2)
 
-    new_data = pd.merge(data, df_notas['freq_rel'], how='left', left_on='idPerfiles', right_index=True)
+    new_df = pd.merge(df, df_notas['freq_rel'], how='left', left_on='idPerfiles', right_index=True)
 
-    new_data['sqr_old'] = new_data['sqr']
+    new_df['sqr_old'] = new_df['sqr']
 
-    new_data.loc[(new_data['freq_rel'].notnull()) & (new_data['notaUsuario'].notnull()), 'sqr'] = new_data['freq_rel']*new_data['notaUsuario'] + (1-new_data['freq_rel'])*new_data['sqr_old']
+    new_df.loc[(new_df['freq_rel'].notnull()) & (new_df['notaUsuario'].notnull()), 'sqr'] = new_df['freq_rel']*new_df['notaUsuario'] + (1-new_df['freq_rel'])*new_df['sqr_old']
 
     if drop:
-        new_data.drop(columns=('freq_rel', 'old_sqr'))
+        new_df.drop(columns=('freq_rel', 'old_sqr'))
 
-    return new_data
+    return new_df
 
 
 
